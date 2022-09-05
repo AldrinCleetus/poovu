265b1f5b7a62039819d8c57a4b6c12217f5cb194
Aldrin <cleetusaldrin30@gmail.com>
2022-09-05 16:31:03 +0530

    improved landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -17,13 +17,13 @@ const LandingPage = () => {
                         Generate Beautiful Pokkal Designs
                         </div>
 
-                        <div className="is-size-4-fullhd is-size-5-mobile">
+                        <div className="is-size-3-fullhd is-size-5-mobile">
                         Using our custom pokkalam design tool create beautiful designs easily 
                         </div>
                         <div className="column has-text-left mt-3 m-0 p-0">
                         
-                            <button className="button CButton is-primary is-rounded mr-4 is-size-5">Gallery</button>
-                            <button className="button CButton is-primary is-rounded ml-4 is-size-5">Create</button>
+                            <button className="button CButton is-primary customRounded px-6 mr-4 is-size-3">Gallery</button>
+                            <button className="button CButton is-primary customRounded px-6 ml-4 is-size-3">Create</button>
                         
                         </div>
                         <div className="column"></div>
@@ -33,21 +33,23 @@ const LandingPage = () => {
                             name={"John Doe"}
                             date={"05/08/22"}
                             image={"images/test.png"}></PooklamCards>
+                             
                              <PooklamCards
                             name={"Jane Doe"}
                             date={"04/08/22"}
                             image={"images/test2.png"}></PooklamCards>
+                            
                         </div>
                         
                     </div>
                    
-                    <div className="column landingImage is-full has-text-centered">
+                    <div className="column landingImage is-half has-text-right">
+                        <img src="../images/illustration.png" alt="te"/>
                         <div>
                             {/* <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.drivemag.net%2Fmedia%2Fdefault%2F0001%2F95%2Fseries-1-ford-mustang-rtr-1-2280-default-large.jpeg&f=1&nofb=1" alt=""
                             width={900} /> */}
 
-                            <img src="../images/illustration.png" alt="te"
-                            width={650} />
+                            
                         </div>
                     </div>
                     
